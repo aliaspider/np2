95ef751d4b2b20e816f64d7d4ce9e339a80b516b
yui <yui@7c2116fe-53d0-4aec-ab59-80607f05abc0>
2004-08-16 15:46:06 +0000

    VER_0_80

---
+++ np2/tags/VER_0_80/i386c/dmap.h
@@ -0,0 +1,11 @@
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void dmap(void);
+
+#ifdef __cplusplus
+}
+#endif
+
---
+++ np2/tags/VER_0_80/win9x/dialog/d_about.cpp
@@ -0,0 +1,109 @@
+#include	"compiler.h"
+#include	"resource.h"
+#include	"np2.h"
+#include	"np2class.h"
+#include	"dialog.h"
+#include	"dialogs.h"
+#include	"np2ver.h"
+#include	"pccore.h"
+#include	"np2info.h"
+
+
+static	SIZE	aboutsize;
+
+#if !defined(SUPPORT_PC9821)
+static const char str_np2title[] = "Neko Project II  ";
+#else
+static const char str_np2title[] = "Neko Project 21  ";
+#endif
+static const char np2infostr[] = 								\
+						"CPU: %CPU% %CLOCK%\n"					\
+						"MEM: %MEM1%\n"							\
+						"GDC: %GDC%\n"							\
+						"     %GDC2%\n"							\
+						"TEXT: %TEXT%\n"						\
+						"GRPH: %GRPH%\n"						\
+						"SOUND: %EXSND%\n"						\
+						"\n"									\
+						"BIOS: %BIOS%\n"						\
+						"RHYTHM: %RHYTHM%\n"					\
+						"\n"									\
+						"SCREEN: %DISP%";
+
+
+static void about_init(HWND hWnd) {
+
+	char	work[128];
+	RECT	rectwindow;
+	RECT	rectclient;
+	POINT	pt;
+	RECT	parent;
+
+	milstr_ncpy(work, str_np2title, sizeof(work));
+	milstr_ncat(work, np2version, sizeof(work));
+#if defined(NP2VER_WIN9X)
+	milstr_ncat(work, NP2VER_WIN9X, sizeof(work));
+#endif
+	SetDlgItemText(hWnd, IDC_NP2VER, work);
+	GetWindowRect(hWnd, &rectwindow);
+	GetClientRect(hWnd, &rectclient);
+	aboutsize.cx = rectwindow.right - rectwindow.left;
+	aboutsize.cy = rectwindow.bottom - rectwindow.top;
+	pt.x = 0;
+	pt.y = 0;
+	ClientToScreen(GetParent(hWnd), &pt);
+	GetClientRect(GetParent(hWnd), &parent);
+	np2class_move(hWnd,
+					pt.x + ((parent.right - parent.left - aboutsize.cx) / 2),
+					pt.y + ((parent.bottom - parent.top - aboutsize.cy) / 2),
+					aboutsize.cx,
+					aboutsize.cy + 60 - (rectclient.bottom - rectclient.top));
+
+	SetFocus(GetDlgItem(hWnd, IDOK));
+}
+
+static void about_more(HWND hWnd) {
+
+	char	infostr[1024];
+	RECT	rect;
+
+	np2info(infostr, np2infostr, sizeof(infostr), NULL);
+	SetDlgItemText(hWnd, IDC_NP2INFO, infostr);
+	EnableWindow(GetDlgItem(hWnd, IDC_MORE), FALSE);
+	GetWindowRect(hWnd, &rect);
+	np2class_move(hWnd, rect.left, rect.top, aboutsize.cx, aboutsize.cy);
+	SetFocus(GetDlgItem(hWnd, IDOK));
+}
+
+LRESULT CALLBACK AboutDialogProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
+
+	switch (msg) {
+		case WM_INITDIALOG:
+			about_init(hWnd);
+			return(FALSE);
+
+		case WM_COMMAND:
+			switch (LOWORD(wp)) {
+				case IDOK:
+					EndDialog(hWnd, IDOK);
+					break;
+
+				case IDC_MORE:
+					about_more(hWnd);
+					break;
+
+				default:
+					return(FALSE);
+			}
+			break;
+
+		case WM_CLOSE:
+			PostMessage(hWnd, WM_COMMAND, IDOK, 0);
+			break;
+
+		default:
+			return(FALSE);
+	}
+	return(TRUE);
+}
+
